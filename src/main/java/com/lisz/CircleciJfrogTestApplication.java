af3782841074927e3178b09cb8921942d5deef75
lisz1012 <lisz1012@163.com>
2023-01-20 21:44:00 -0800

    Initial commit

---
+++ src/main/java/com/lisz/CircleciJfrogTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.circlecijfrogtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleciJfrogTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircleciJfrogTestApplication.class, args);
+    }
+
+}
