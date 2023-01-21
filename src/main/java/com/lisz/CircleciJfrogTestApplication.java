1e45cecb0dc767874df4012ac04affe94aa48cfb
lisz1012 <lisz1012@163.com>
2023-01-21 00:46:59 -0800

    remove unecessary code

--- src/main/java/com/lisz/CircleciJfrogTestApplication.java
+++ src/main/java/com/lisz/CircleciJfrogTestApplication.java
@@ -1,13 +1,9 @@
 package com.lisz;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class CircleciJfrogTestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CircleciJfrogTestApplication.class, args);
+        System.out.println("hello, world");
     }
 
 }
