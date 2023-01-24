ea16c1a31764c6a7cbee66a57e041341c201b3dc
lisz1012 <lisz1012@163.com>
2023-01-23 20:17:44 -0800

    Add main method

--- src/main/java/com/lisz/CircleciJfrogTestApplication.java
+++ src/main/java/com/lisz/CircleciJfrogTestApplication.java
@@ -4,4 +4,8 @@ public class CircleciJfrogTestApplication {
     public void m() {
         System.out.println("hotfix");
     }
+
+    public static void main(String[] args) {
+        System.out.println("dev!!");
+    }
 }
